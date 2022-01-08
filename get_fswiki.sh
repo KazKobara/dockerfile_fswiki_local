7095185984156b487a3fd3342ab25942f01c2be0
Kaz KOBARA <M-kaz-ml@aist.go.jp>
2022-01-08 16:48:52 +0900

    Fix a typo in patch name

--- get_fswiki.sh
+++ get_fswiki.sh
@@ -2,7 +2,7 @@
 # usage:
 #   $ ./get_fswiki.sh [FSWIKI_VERSION [FSWIKI_TMP_DIR]]
 #
-#   where fefault FSWIKI_VERSION and FSWIKI_TMP_DIR are given in .env file.
+#   where default FSWIKI_VERSION and FSWIKI_TMP_DIR are given in .env file.
 
 source .env
 echo "FSWIKI_VERSION: $FSWIKI_VERSION"
@@ -22,16 +22,16 @@ PATCH_COMMAND="git --git-dir= apply"
 
 apply_patches () {
  	mkdir -p ../patches
-    ## Check 'serch in content' check box with config/config.dat.
+    ## Check 'search in content' check box with config/config.dat.
     ## https://fswiki.osdn.jp/cgi-bin/wiki.cgi?page=BugTrack%2Drequest%2F109
-    if [ ! -e ../patches/serch_in_content_control_in_config_dat.patch ]; then
-	    curl -o ../patches/serch_in_content_control_in_config_dat.patch "http://fswiki.osdn.jp/cgi-bin/wiki.cgi?page=BugTrack%2Drequest%2F109&file=serch%5Fin%5Fcontent%5Fcontrol%5Fin%5Fconfig%5Fdat%2Epatch&action=ATTACH"
+    if [ ! -e ../patches/search_in_content_control_in_config_dat.patch ]; then
+	    curl -o ../patches/search_in_content_control_in_config_dat.patch "https://fswiki.osdn.jp/cgi-bin/wiki.cgi?page=BugTrack%2Drequest%2F109&file=search%5Fin%5Fcontent%5Fcontrol%5Fin%5Fconfig%5Fdat%2Epatch&action=ATTACH"
     fi
-    $PATCH_COMMAND ../patches/serch_in_content_control_in_config_dat.patch
+    $PATCH_COMMAND ../patches/search_in_content_control_in_config_dat.patch
     ## Patch for 'value or values' warning.
     ## https://fswiki.osdn.jp/cgi-bin/wiki.cgi?page=BBS%2D%A5%B5%A5%DD%A1%BC%A5%C8%B7%C7%BC%A8%C8%C4%2F997
     if [ ! -e ../patches/value_or_values_in_lib_CGI2_pm.patch ]; then
-	    curl -o ../patches/value_or_values_in_lib_CGI2_pm.patch "http://fswiki.osdn.jp/cgi-bin/wiki.cgi?action=ATTACH&file=value%5For%5Fvalues%5Fin%5Flib%5FCGI2%5Fpm%2Epatch&page=BBS%2D%A5%B5%A5%DD%A1%BC%A5%C8%B7%C7%BC%A8%C8%C4%2F997"
+	    curl -o ../patches/value_or_values_in_lib_CGI2_pm.patch "https://fswiki.osdn.jp/cgi-bin/wiki.cgi?action=ATTACH&file=value%5For%5Fvalues%5Fin%5Flib%5FCGI2%5Fpm%2Epatch&page=BBS%2D%A5%B5%A5%DD%A1%BC%A5%C8%B7%C7%BC%A8%C8%C4%2F997"
     fi
     $PATCH_COMMAND ../patches/value_or_values_in_lib_CGI2_pm.patch
     ## Add Menu.wiki
@@ -42,7 +42,7 @@ echo
 echo "=== getting or updating fswiki ==="
 mkdir -p "${FSWIKI_TMP_DIR}"
 # pushd/popd is needed since this is sourced by ./docker_build.sh.
-pushd "$(pwd)"
+pushd "$(pwd)" || exit 2
 cd "${FSWIKI_TMP_DIR}/" || { echo "ERROR: could not 'cd ${FSWIKI_TMP_DIR}/'!!"; exit 1; }
 if [ "$FSWIKI_VERSION" == "latest" ]; then
     if [ ! -e "${FSWIKI_SOURCE_DIR}" ]; then
@@ -67,14 +67,15 @@ if [ "${FSWIKI_THEME}" == "kati_dark" ]; then
     echo
     echo "=== git clone or pull 'kati_dark' theme ==="
     # pushd/popd is needed since this is sourced by ./docker_build.sh.
-    pushd "$(pwd)"
+    pushd "$(pwd)" || exit 2
+    mkdir -p "${FSWIKI_SOURCE_DIR}/theme"
     cd "${FSWIKI_SOURCE_DIR}/theme" || { echo "ERROR: could not 'cd ${FSWIKI_SOURCE_DIR}/wiki${FSWIKI_VERSION}/theme/'!!"; exit 1; }
     if [ ! -e "${FSWIKI_THEME}" ]; then
         git clone git@github.com:KazKobara/kati_dark.git
     else
         (cd "kati_dark" && git pull)
     fi
-    popd
+    popd || exit 2
 fi
 
 # Change theme of FSWiki
@@ -92,4 +93,4 @@ if [ "$?" == "1" ]; then
     echo "------------------------------"
 fi
 
-popd	
+popd || exit 2
